e003287c4376c6b4b2f16114ab1ad4277c860e2e
igings <igings@hotmail.co.uk>
2014-07-25 20:23:42 +0100

    Initial Commit

--- src/main/java/com/igings/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/igings/letsmodreboot/LetsModReboot.java
@@ -1,24 +1,25 @@
 package com.igings.letsmodreboot;
 
 import com.igings.letsmodreboot.proxy.IProxy;
+import com.igings.letsmodreboot.reference.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="LetsModReboot",name="Lets Mod Reboot", version="1.7.2-1.0")
+@Mod(modid= Reference.MOD_ID,name=Reference.MOD_NAME, version=Reference.VERSION)
 public class LetsModReboot
 {
-    @Mod.Instance("LetsModReboot")
+    @Mod.Instance(Reference.MOD_ID)
     public static LetsModReboot instance;
 
-    @SidedProxy(clientSide="com.igings.letsmodreboot.proxy.ClientProxy",serverSide = "com.igings.letsmodreboot.proxy.ServerProxy")
+    @SidedProxy(clientSide=Reference.CLIENT_PROXY_CLASS,serverSide = Reference.SERVER_PROXY_CLASS)
     public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
-    {//Initialisation for items, blocks, keybinds, network, etc
+    {//Initialisation for items, blocks, key-binds, network, etc
 
     }
 
--- src/main/java/com/igings/letsmodreboot/reference/Reference.java
+++ src/main/java/com/igings/letsmodreboot/reference/Reference.java
@@ -1,11 +1,10 @@
 package com.igings.letsmodreboot.reference;
 
-/**
- * Created by Sam on 17/07/2014.
- */
 public class Reference
 {
     public static final String MOD_ID="LetsModReboot";
     public static final String MOD_NAME="Lets Mod Reboot";
     public static final String VERSION="@VERSION@";
+    public static final String CLIENT_PROXY_CLASS="com.igings.letsmodreboot.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS="com.igings.letsmodreboot.proxy.ServerProxy";
 }
